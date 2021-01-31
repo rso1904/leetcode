f8d14355bd5d0af494bbfdee1c8a20936793c18c
rso1904 <rsw1904@gmail.com>
2021-02-01 02:51:21 +0900

    Time: 188 ms (27.27%), Space: 42.8 MB (9.09%) - LeetHub

--- palindrome-partitioning-iv/palindrome-partitioning-iv.java
+++ palindrome-partitioning-iv/palindrome-partitioning-iv.java
@@ -2,56 +2,12 @@ class Solution {
     public boolean checkPartitioning(String s) {
         boolean[][] dp = new boolean[s.length()][s.length()];
         
-        for(int i=0; i < s.length(); i++) {
-            dp[i][i] = true;
-        }
-        
-        for(int i=1; i < s.length()-1; i++) {
-            int left = i-1;
-            int right = i+1;
-            
-            while(left >= 0 && right < s.length()) {
-                if(s.charAt(left) == s.charAt(right)) {
-                    dp[left][right] = dp[left+1][right-1];
+        for(int i=s.length()-1; i >= 0; i--) {
+            for(int j=i; j < s.length(); j++) {
+                if(s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = i+1 <= j-1 ? dp[i+1][j-1] : true;
                 } else {
-                    break;
-                }
-                
-                left--;
-                right++;
-            }
-            
-            if(s.charAt(i) == s.charAt(i-1)) {
-                dp[i-1][i] = true;
-                left = i-2;
-                right = i+1;
-                
-                while(left >= 0 && right < s.length()) {
-                    if(s.charAt(left) == s.charAt(right)) {
-                        dp[left][right] = dp[left+1][right-1];
-                    } else {
-                        break;
-                    }
-                    
-                    left--;
-                    right++;
-                }
-            }
-            
-            if(s.charAt(i) == s.charAt(i+1)) {
-                dp[i][i+1] = true;
-                left = i-1;
-                right = i+2;
-                
-                while(left >= 0 && right < s.length()) {
-                    if(s.charAt(left) == s.charAt(right)) {
-                        dp[left][right] = dp[left+1][right-1];
-                    } else {
-                        break;
-                    }
-                    
-                    left--;
-                    right++;
+                    dp[i][j] = false;
                 }
             }
         }
