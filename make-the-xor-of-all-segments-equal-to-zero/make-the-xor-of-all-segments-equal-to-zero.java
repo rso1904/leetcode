a1075c6b1e851b59b6fd9e6dafec8aee37e451c9
rso1904 <rsw1904@gmail.com>
2021-03-17 01:46:11 +0900

    Time: 167 ms (100.00%), Space: 64.8 MB (100.00%) - LeetHub

--- make-the-xor-of-all-segments-equal-to-zero/make-the-xor-of-all-segments-equal-to-zero.java
+++ make-the-xor-of-all-segments-equal-to-zero/make-the-xor-of-all-segments-equal-to-zero.java
@@ -1,14 +1,14 @@
 class Solution {
     public int minChanges(int[] nums, int k) {
-        int[][] dp = new int[k+1][1025];
-        int[][] freq = new int[k][1025];
+        int[][] dp = new int[k+1][1024];
+        int[][] freq = new int[k][1024];
         
         for(int i=0; i < nums.length; i++) {
             freq[i%k][nums[i]]++;
         }
         
         for(int i=0; i < k+1; i++) {
-            for(int j=0; j < 1025; j++) {
+            for(int j=0; j < 1024; j++) {
                 dp[i][j] = 2048;
             }
         }
@@ -20,9 +20,9 @@ class Solution {
             int sizeOfPos = (int)Math.ceil((nums.length - i) * 1.0 / k);
             int curMin = Integer.MAX_VALUE;
             
-            for(int j=0; j < 1025; j++) {
+            for(int j=0; j < 1024; j++) {
                 for(int z=i; z < nums.length; z += k) {
-                    if((j ^ nums[z]) < 1025) {
+                    if((j ^ nums[z]) < 1024) {
                         dp[i+1][j ^ nums[z]] = Math.min(dp[i+1][j ^ nums[z]], dp[i][j] + sizeOfPos - freq[i][nums[z]]);
                     }
                 }
