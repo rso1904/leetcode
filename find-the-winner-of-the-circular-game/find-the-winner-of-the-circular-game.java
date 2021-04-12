c2eb3702133c254ec522510967fd920e31297b07
rso1904 <rsw1904@gmail.com>
2021-04-13 01:56:45 +0900

    Time: 0 ms (100.00%), Space: 35.6 MB (100.00%) - LeetHub

--- find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,18 +1,11 @@
 class Solution {
     public int findTheWinner(int n, int k) {
-        LinkedList<Integer> list = new LinkedList<>();
-        int index = k - 1;
+        int res = 0;
         
         for(int i=1; i <= n; i++) {
-            list.add(i);
+            res = (res + k) % i;
         }
         
-        while(list.size() > 1) {
-            list.remove(index);
-            
-            index = (index - 1 + k) % list.size();
-        }
-        
-        return list.get(0);
+        return res+1;
     }
 }
