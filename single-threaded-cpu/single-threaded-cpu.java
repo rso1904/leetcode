14e6a8812c174361da0e76da9a985ee534a53973
rso1904 <rsw1904@gmail.com>
2021-04-19 01:09:45 +0900

    Time: 164 ms (14.29%), Space: 87.5 MB (14.29%) - LeetHub

--- single-threaded-cpu/single-threaded-cpu.java
+++ single-threaded-cpu/single-threaded-cpu.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] getOrder(int[][] tasks) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        PriorityQueue<int[]> pq1 = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        PriorityQueue<int[]> pq1 = new PriorityQueue<>((a, b) -> a[1] == b[1] ? a[2] - b[2] : a[1] - b[1]);
         int time = 0;
         int[] res = new int[tasks.length];
         int index = 0;
@@ -11,41 +11,19 @@ class Solution {
         }
         
         while(!pq.isEmpty() || !pq1.isEmpty()) {
-            if(time == 0) {
-                int[] start = pq.poll();
-                time += start[0];
-                pq1.offer(start);
-                continue;
-            }
-
             while (!pq.isEmpty() && pq.peek()[0] <= time) {
                 pq1.offer(pq.poll());
             } 
             
             if(!pq1.isEmpty()) {
                 int[] item = pq1.poll();
-                List<int[]> temp = new ArrayList<>();
-                
-                while(!pq1.isEmpty() && item[1] == pq1.peek()[1]) {
-                    if(pq1.peek()[2] < item[2]) {
-                        temp.add(item);
-                        item = pq1.poll();
-                    } else {
-                        temp.add(pq1.poll());
-                    }
-                }
                 
                 time += item[1];
                 res[index++] = item[2];
-                
-                for(int[] t : temp) {
-                    pq1.offer(t);
-                }
             } else {
                 int[] start = pq.poll();
                 time += start[0];
                 pq1.offer(start);
-                continue;
             }
         }
         
