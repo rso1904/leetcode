e4de54ef46957350fa20b0c49b2700a249abb48e
rso1904 <rsw1904@gmail.com>
2021-10-11 21:02:21 +0900

    Time: 26 ms (10.35%), Space: 41.4 MB (32.68%) - LeetHub

---
+++ flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int[][] left = new int[2][s.length()];
+        int[][] right = new int[2][s.length()];
+        int res = Integer.MAX_VALUE;
+        
+        for(int i=0; i < s.length(); i++) {
+            if(i == 0) {
+                if(s.charAt(i) == '0') {
+                    left[0][i] = 0;
+                    left[1][i] = 1;
+                } else {
+                    left[0][i] = 1;
+                    left[1][i] = 0;
+                }
+            } else {
+                if(s.charAt(i) == '0') {
+                    left[0][i] = left[0][i-1];
+                    left[1][i] = left[1][i-1] + 1;
+                } else {
+                    left[0][i] = left[0][i-1] + 1;
+                    left[1][i] = left[1][i-1];
+                }
+            }
+        }
+        
+        for(int i=s.length()-1; i >= 0; i--) {
+            if(i == s.length()-1) {
+                if(s.charAt(i) == '0') {
+                    right[0][i] = 0;
+                    right[1][i] = 1;
+                } else {
+                    right[0][i] = 1;
+                    right[1][i] = 0;
+                }
+            } else {
+                if(s.charAt(i) == '0') {
+                    right[0][i] = right[0][i+1];
+                    right[1][i] = right[1][i+1] + 1;
+                } else {
+                    right[0][i] = right[0][i+1] + 1;
+                    right[1][i] = right[1][i+1];
+                }
+            }
+        }
+        
+        for(int i=0; i < s.length()-1; i++) {
+            res = Math.min(res, left[0][i]+right[1][i+1]);
+            res = Math.min(res, left[0][i]+right[0][i+1]);
+            res = Math.min(res, left[0][i]+right[1][i+1]);
+        }
+        
+        res = Math.min(res, left[0][s.length()-1]);
+        res = Math.min(res, left[1][s.length()-1]);
+        
+        return res;
+    }
+}
