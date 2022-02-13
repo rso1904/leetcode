741f65d14b6be507e490cc5e70d5b0d93236e85c
rso1904 <rsw1904@gmail.com>
2022-02-14 01:52:19 +0900

    Time: 43 ms (33.33%), Space: 42.6 MB (66.67%) - LeetHub

---
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[] mem;
+    int[] size;
+    
+    public int maximumANDSum(int[] nums, int numSlots) {
+        int mask = (int)Math.pow(3, numSlots) - 1;
+        mem = new int[mask+1];
+        size = new int[numSlots+1];
+        
+        return search(nums, 0, 0, numSlots);
+    }
+    
+    private int search(int[] nums, int cur, int mask, int numSlots) {
+        if(cur >= nums.length) {
+            return 0;
+        }
+        if(mem[mask] > 0) {
+            return mem[mask];
+        }
+        
+        int res = 0;
+        
+        for(int i=1; i <= numSlots; i++) {
+            if(size[i] < 2) {
+                size[i]++;
+                
+                res = Math.max(res, search(nums, cur+1, mask + (int)Math.pow(3, i-1), numSlots) + (i & nums[cur]));
+                size[i]--;
+            } 
+        }
+        
+        mem[mask] = res;
+        return res;
+    }
+}
