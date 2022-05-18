5155671a39cb97cdd1d0c45591be102f54f4d544
rso1904 <rsw1904@gmail.com>
2022-05-19 02:55:49 +0900

    Time: 402 ms (5.07%), Space: 136 MB (77.59%) - LeetHub

--- 2276-count-integers-in-intervals/2276-count-integers-in-intervals.java
+++ 2276-count-integers-in-intervals/2276-count-integers-in-intervals.java
@@ -7,7 +7,6 @@ class CountIntervals {
     }
     
     public void add(int left, int right) {
-    //    System.out.println(left + " : " + right + " : " + count);
         while(treeMap.ceilingEntry(left) != null && treeMap.ceilingEntry(left).getValue() <= right) {
             count -= treeMap.ceilingEntry(left).getValue() - treeMap.ceilingEntry(left).getKey() + 1;
             treeMap.remove(treeMap.ceilingEntry(left).getKey());
@@ -19,15 +18,13 @@ class CountIntervals {
         }
         
         if(treeMap.floorEntry(left) != null && treeMap.ceilingEntry(left) != null && treeMap.floorEntry(left).getKey() <= left && treeMap.floorEntry(left).getValue() <= right && treeMap.floorEntry(left).getValue() >= left && treeMap.ceilingEntry(left).getKey() >= left && treeMap.ceilingEntry(left).getValue() >= right && treeMap.ceilingEntry(left).getKey() <= right) {
-        //    System.out.println("hi");
             int l = treeMap.floorEntry(left).getKey();
             int r = treeMap.ceilingEntry(left).getValue();
             int rE = treeMap.ceilingEntry(left).getKey();
-     //       System.out.println(l + " : " + treeMap.floorEntry(left).getValue() + " : " + treeMap.ceilingEntry(left).getKey() + " : "+  r);
+
             count -= treeMap.floorEntry(left).getValue() - treeMap.floorEntry(left).getKey() + 1;
-     //       System.out.println(count);
             count -= treeMap.ceilingEntry(left).getValue() - treeMap.ceilingEntry(left).getKey() + 1;
-    //        System.out.println(count);
+            
             count += r - l + 1;
             treeMap.put(l, r);
             treeMap.remove(rE);
@@ -43,7 +40,6 @@ class CountIntervals {
             count += right - left + 1;
             treeMap.put(left, right);
         }
-  //      System.out.println(left + " : " + right + " : " + count);
     }
     
     public int count() {
